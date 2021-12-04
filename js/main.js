b417990bde19472fe8a7ba8800a0e25bd1763211
Olia Dovbush <oliaivanchushun@gmail.com>
2021-12-04 13:28:29 +0200

    add view.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
+import {
+	inputTaskHigh, elementHighForm, highPriorityTask, inputTaskLow, elementLowForm, lowPriorityTask,
+	doneButtons, deleteButtons
+} from './view.js'
 
-const addButton = document.getElementById('add');
-let inputTask = document.getElementById('new-task');
-const elementForm = document.getElementById('form');
-const highPriorityTask = document.getElementById('high-priority-task');
+let id = 4;
 
 function createNewElement(task) {
 	let taskElement = document.createElement('li');
 	let checkButton = document.createElement('input');
 	checkButton.type = 'checkbox';
 	checkButton.classList.add('checkbox');
+	checkButton.setAttribute('id', `${++id}`);
+
 	let label = document.createElement('label');
+	label.setAttribute('for', `${id}`);
 	label.innerText = task;
+
 	let deleteButton = document.createElement('input');
 	deleteButton.type = 'button';
 	deleteButton.classList.add('task-button');
@@ -19,17 +24,65 @@ function createNewElement(task) {
 	taskElement.appendChild(label);
 	taskElement.appendChild(deleteButton);
 
+	checkButton.addEventListener('click', function (event) {
+		changeStatus(event.target);
+	})
+
+	deleteButton.addEventListener('click', function (event) {
+		deleteTask(event.target);
+	})
+
 	return taskElement;
 }
 
-function addTask() {
-	if (inputTask.value) {
-		let taskElement = createNewElement(inputTask.value);
+function addHighTask() {
+	if (inputTaskHigh.value) {
+		let taskElement = createNewElement(inputTaskHigh.value);
 		highPriorityTask.appendChild(taskElement);
-		inputTask.value = '';
+
+		inputTaskHigh.value = '';
 	}
 }
 
-addButton.addEventListener('click', function (event) {
-	addTask();
+elementHighForm.addEventListener('submit', function (event) {
+	event.preventDefault();
+	addHighTask();
 })
+
+function addLowTask() {
+	if (inputTaskLow.value) {
+		let taskElement = createNewElement(inputTaskLow.value);
+		lowPriorityTask.appendChild(taskElement);
+		inputTaskLow.value = '';
+	}
+}
+
+elementLowForm.addEventListener('submit', function (event) {
+	event.preventDefault();
+	addLowTask();
+})
+
+
+for (const button of doneButtons) {
+	button.addEventListener('click', function (event) {
+		changeStatus(event.target);
+	})
+}
+
+function changeStatus(button) {
+	button.parentElement.classList.toggle('changebg');
+}
+
+for (const button of deleteButtons) {
+	button.addEventListener('click', function (event) {
+		deleteTask(event.target);
+	})
+}
+
+function deleteTask(button) {
+	button.parentElement.remove();
+}
+
+
+
+
---
+++ js/view.js
@@ -0,0 +1,10 @@
+export const inputTaskHigh = document.getElementById('new-task');
+export const elementHighForm = document.getElementById('form');
+export const highPriorityTask = document.getElementById('high-priority-task');
+export const inputTaskLow = document.getElementById('new-task2');
+export const elementLowForm = document.getElementById('form2');
+export const lowPriorityTask = document.getElementById('low-priority-task');
+
+export const doneButtons = document.querySelectorAll('.checkbox');
+
+export const deleteButtons = document.querySelectorAll('.task-button');
